46731662eb6388097c78bb5671f77c4d15dad1a9
Mohamed El Sayed Ahmed Ismail <darkangel7863@gmail.com>
2024-06-05 15:18:41 +0300

    Create star Rating API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// import './index.css';
+// import App from './App';
+import StarRating from './starRating';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating maxRating={10} />
+    <StarRating
+      maxRating={7}
+      color={'#212464'}
+      size={32}
+      messages={[
+        'Perfect',
+        'good',
+        'acceptable',
+        'medium',
+        'weak',
+        'extremely weak',
+        'Ignore',
+      ]}
+      defaultRating={3}
+    />
+    {/* maxRating = 5,
+  color = '#fcc419',
+  size = 48,
+  className = '',
+  messages = [],
+  defaultRating = 0, */}
   </React.StrictMode>
 );
